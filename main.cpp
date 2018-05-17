ea18b02b3fff9c3c6ea86b80f36db5f2e801598e
grisha-feoktistov <g.fioktistov@rambler.ru>
2018-05-18 00:54:01 +0400

    Demonstration controller
    read and write data through the controller

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include "src\Controllers\Controller.h"
-int main() {
+#include <iostream>
+#include <memory>
+//#include "src\DataProviders\DataProvider.h"
+//#include "src\DataConsumers\StreamDataWriter.h"
+
+int main()
+{
+	std::string input_file_name = "demonstration_data/train.csv";
+	std::string output_file_name = "demonstration_data/output.txt";
+	auto provider = std::make_shared<tcc::KaggleDataProvider> (tcc::KaggleDataProvider(input_file_name));
+	auto consumer = std::make_shared<tcc::StreamDataWriter> (tcc::StreamDataWriter(output_file_name));
+	auto controller = tcc::Controller(provider, provider, consumer);
+
+	controller.run();
+
 	return 0;
 }
