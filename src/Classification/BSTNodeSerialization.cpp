e061cf42d075e438904480173a079833acdfd4c3
Georgiy Mescheryakov <herrberg@yandex.ru>
2018-05-27 00:04:06 +0300

    Fixed serialization

--- src/Classification/BSTNodeSerialization.cpp
+++ src/Classification/BSTNodeSerialization.cpp
@@ -15,34 +15,33 @@ void serialize(BSTNode* node, std::ofstream &out)
     }
 }
 
-BSTNode* _deserialize(std::vector<BSTNode*> &nodes, unsigned cur_ind, size_t min, size_t max)
+BSTNode* _deserialize(std::vector<BSTNode*> &nodes, unsigned &cur_ind, size_t min, size_t max)
 {
-    if (cur_ind >= nodes.size() - 1)
+    if (cur_ind >= nodes.size())
     {
         return nullptr;
     }
     BSTNode* node = nodes[cur_ind];
     if (node->w.word_hash > min && node->w.word_hash < max)
     {
-        node->left = _deserialize(nodes, cur_ind + 1, min, node->w.word_hash);
-        node->right = _deserialize(nodes, cur_ind + 1, node->w.word_hash, max);
+        cur_ind++;
+        node->left = _deserialize(nodes, cur_ind, min, node->w.word_hash);
+        node->right = _deserialize(nodes, cur_ind, node->w.word_hash, max);
         return node;
     }
-    else
-    {
-        return nullptr;
-    }
+    return nullptr;
 }
 
 BSTNode* deserialize(std::ifstream &in)
 {
     Word w;
     std::vector<BSTNode*> nodes;
+    unsigned index = 0;
     while(!in.read(&w, sizeof(w)))
     {
         nodes.push_back(new BSTNode(w));
     }
-    _deserialize(nodes, 0, std::numeric_limits<std::size_t>::min(), std::numeric_limits<std::size_t>::max());
+    _deserialize(nodes, index, std::numeric_limits<std::size_t>::min(), std::numeric_limits<std::size_t>::max())->_calc_height();
     return nodes.front();
 }
 
