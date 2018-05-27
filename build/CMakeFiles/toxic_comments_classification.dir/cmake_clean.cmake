146917884a0b61acd67ba273aa893a62aefd1846
George Mesch <herrberg@yandex.ru>
2018-05-27 23:33:57 +0300

    Added CMake

---
+++ build/CMakeFiles/toxic_comments_classification.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toxic_comments_classification.dir/src/Classification/BOW.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/Classification/BSTNode.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/Classification/BSTNodeSerialization.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/Classification/NaiveBayesClassifyer.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/Cores/RandomCore.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/DataProviders/KaggleDataProvider.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/DataProviders/PlainTextDataProvider.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/GUI/mainwindow.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/Stemmer/Stemming.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/cmdParser.cpp.o"
+  "CMakeFiles/toxic_comments_classification.dir/src/main.cpp.o"
+  "toxic_comments_classification.pdb"
+  "toxic_comments_classification"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toxic_comments_classification.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/toxic_comments_classification.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for toxic_comments_classification.
+# This may be replaced when dependencies are built.
